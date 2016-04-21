23f23c09b27c6969db9db68ec2f8380e2a8bfbe8
kotarokibe <exfamily0315@i.softbank.jp>
2016-04-21 14:24:09 +0000

    fix pt2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,11 +37,12 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :area)
   end
   
   def logged_in_user
-    unless logged_in?
+    @user = User.find(params[:id])
+    if @user != current_user
     flash[:danger] = "Please log in!"
     redirect_to login_url
     end
