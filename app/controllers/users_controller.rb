b61f3850ad09bca14e11fefadd3c132c67f69e43
kotarokibe <exfamily0315@i.softbank.jp>
2016-04-25 01:16:54 +0000

    fix pt4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation, :area)
+                                 :password_confirmation, :profile, :area)
   end
   
   def logged_in_user
