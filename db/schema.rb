573c418f359053e300d3b97587bfe4053246bf6b
kotarokibe <exfamily0315@i.softbank.jp>
2016-04-25 00:45:19 +0000

    before fix 4

--- db/schema.rb
+++ db/schema.rb
@@ -23,15 +23,4 @@ ActiveRecord::Schema.define(version: 20160422062031) do
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "area"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-
 end
